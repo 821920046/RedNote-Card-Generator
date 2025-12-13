68f44fecf5ecf4160bfb037ee53760d8e41a9184
Windwalker <38275925+821920046@users.noreply.github.com>
2025-12-13 22:45:39 +0800

    Change base path to relative in vite.config.ts
    Set base path to relative in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // 不要使用绝对路径
   plugins: [react()],
 })
